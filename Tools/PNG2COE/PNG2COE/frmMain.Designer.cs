e9de229c6521be8afb2721313e5cd10de939ef58
Thoralt <thoralt@googlemail.com>
2015-04-03 12:20:26 +0200

    Initial commit

---
+++ Tools/PNG2COE/PNG2COE/frmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PNG2COE
+{
+   partial class frmMain
+   {
+      /// <summary>
+      /// Erforderliche Designervariable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Verwendete Ressourcen bereinigen.
+      /// </summary>
+      /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Vom Windows Form-Designer generierter Code
+
+      /// <summary>
+      /// Erforderliche Methode für die Designerunterstützung.
+      /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.btnGo = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // btnGo
+         // 
+         this.btnGo.Location = new System.Drawing.Point(12, 12);
+         this.btnGo.Name = "btnGo";
+         this.btnGo.Size = new System.Drawing.Size(75, 23);
+         this.btnGo.TabIndex = 0;
+         this.btnGo.Text = "Go";
+         this.btnGo.UseVisualStyleBackColor = true;
+         this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+         // 
+         // frmMain
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(196, 116);
+         this.Controls.Add(this.btnGo);
+         this.Name = "frmMain";
+         this.Text = "PNG2COE";
+         this.Shown += new System.EventHandler(this.frmMain_Shown);
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button btnGo;
+   }
+}
+
---
+++ Tools/PNG2COE/PNG2COE/frmMain.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PNG2COE
+{
+   public partial class frmMain : Form
+   {
+      public frmMain()
+      {
+         InitializeComponent();
+      }
+
+      private Boolean convertFile(String inputFile, String outputFile)
+      {
+         try
+         {
+            if(File.Exists(outputFile))
+            {
+               if(MessageBox.Show("The destination file " + outputFile + " already exists.\n\nOverwrite?", 
+                  "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+               {
+                  return false;
+               }
+            }
+            TextWriter output = File.CreateText(outputFile);
+            Bitmap bm = new Bitmap(inputFile);
+            output.WriteLine("memory_initialization_radix=2;");
+            output.WriteLine("memory_initialization_vector=");
+            for (int y = 0; y < bm.Height; y++)
+            {
+               for (int x = 0; x < bm.Width; x++)
+               {
+                  Color c = bm.GetPixel(x, y);
+                  if (c.R == 0 && c.G == 0 && c.B == 0)
+                  {
+                     output.Write("0");
+                  }
+                  else
+                  {
+                     output.Write("1");
+                  }
+                  if (x != bm.Width && y != bm.Height) output.Write(",");
+               }
+               if (y != bm.Height) output.WriteLine();
+            }
+            output.WriteLine(";");
+            output.Close();
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+         }
+         return true;
+      }
+
+      private void startConversion()
+      {
+         try
+         {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image Files(*.bmp;*.jpg;*.gif;*.png)|*.bmp;*.jpg;*.gif;*.png|All files (*.*)|*.*";
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+            String outputFile = Path.GetDirectoryName(dlg.FileName)
+               + "\\" + Path.GetFileNameWithoutExtension(dlg.FileName) + ".coe";
+            if(convertFile(dlg.FileName, outputFile))
+            {
+               MessageBox.Show("The converted file has been written to " + outputFile,
+                  "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+         }
+      }
+
+      private void btnGo_Click(object sender, EventArgs e)
+      {
+         startConversion();
+      }
+
+      private void frmMain_Shown(object sender, EventArgs e)
+      {
+         startConversion();
+      }
+   }
+}
